fc8bb4a769c5284a9d6ba336e9e4fc436df46457
shivam <jadhavshivam0228@gmail.com>
2020-05-01 14:34:40 +0530

    Linear Regression Model

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,26 +1,39 @@
+#Linear Regression model from scratch
+
+
+
+#Importing essential libraries
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Importing dataset to build model 
 
 dataset=pd.read_csv("Salary_Data.csv")
+
+#Taking Dependent and Independent variables
+
 X=dataset.iloc[:,0].values
 Y=dataset.iloc[:,-1].values
 
-xmean=np.mean(X)
-ymean=np.mean(Y)
+# following general form Y=Mx+C to find Correlation 
+# Y is dependent variable M is Slope and C is intercept
+# m=sum((y-y^)*(x-x^))/sum((x-x^)**2) """
 
-nume=0
-denom=0
+x_mean=np.mean(X)
+y_mean=np.mean(Y)
+
+numerator=0
+denominator=0
 for i in range(30):
-    nume+=((X[i]-xmean)*(Y[i]-ymean))
-    denom+=(X[i]-xmean)**2
-M=nume/denom
-C=ymean-M*xmean
-y=M*x+C
+    numerator+=((X[i]-x_mean)*(Y[i]-y_mean))
+    denominator+=(X[i]-x_mean)**2
+M=numerator/denominator
+C=y_mean-M*x_mean
 
 
-
+# Building Model
 
 max_x=np.max(X)
 min_x=np.min(X)
@@ -29,6 +42,7 @@ x=np.linspace(min_x,max_x,1000)
 y=M*x+C
 
 
+# Visualising data
 
 plt.plot(x,y,color="purple")
 plt.scatter(X,Y,color="red")
@@ -38,15 +52,3 @@ plt.ylabel('Salary')
 plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
-# Fitting Linear Regression to the dataset
